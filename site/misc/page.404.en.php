ed5a1a73174f5a1aee5d2c3326195a212de0218c
Jens Klingen <jklingen@users.sourceforge.net>
2013-10-05 12:59:43 +0200

    added custumizable 404 Not Found page

---
+++ site/misc/page.404.en.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+${config
+    id=   404
+    name= Page not found
+}$
+ */
+
+/**
+ * Custom 404 page, feel free to change it to fit your needs.
+ * Note: won't be displayed when PHeasel is running in development environment, because the error page
+ * contains more background information.
+ */
+?>
+<html>
+<head>
+
+    <title>Page not found</title>
+    <meta name="robots" content="noindex,follow"/>
+</head>
+<body>
+<h1>Oooops - Page not found :(</h1>
+<p>Sorry, we could not find the page you are looking for.</p>
+</body>
+</html>
