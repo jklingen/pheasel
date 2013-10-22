22a0e8016213c93711247166b3592cd874f79b0d
jklingen <jklingen@users.sourceforge.net>
2013-10-22 22:12:49 +0200

    improved handling for exported pages + added documentation

--- site/misc/page.404.en.php
+++ site/misc/page.404.en.php
@@ -1,14 +1,27 @@
 <?php
 /**
  * Custom 404 page, feel free to change it to fit your needs.
- * Note: won't be displayed when PHeasel is running in development environment, because the error page
- * contains more background information.
+ *
+ * Note (1): shows some helpful additional information in DEV mode.
+ *
+ * Note (2): When exporting as HTML or PHP, this page will be exported e.g. to 404.html or 404.php, respectively.
+ * But that does not mean that it will automatically be used when a page is not found.
+ *
+ * If your web server has mod_rewrite enabled you need to add an .htaccess file specifying
+ * ErrorDocument 404 /404.html
+ *
+ * If it does not, please get in touch with your hoster, there are probably has file name conventions for 404 pages,
+ * e.g. that they must be called 404.html.
+ *
+ * Note (3): when used as described in (2), you might see broken links or references to javascript/stylesheets on
+ * this page. If this is the case, please edit pheasel-config.php and insert a value for PHEASEL_CONTEXT_PATH, so
+ * that the RequestHandler can insert references relative to the webserver root.
  */
 ?>
-
+<!DOCTYPE html>
 <html>
 <head>
-	<ph:config id="404" name="Page not found" />
+	<ph:config id="404" name="Page not found" template="pheasel" />
     <title>Page not found</title>
     <meta name="robots" content="noindex,follow"/>
 </head>
@@ -18,31 +31,33 @@
 <?php
     // try to find best matching URL for suggestion
     $url = PageInfo::$current->url;
-    $pis = SiteConfig::get_instance()->get_all_page_infos();
-    $best_match_pi = null;
-    $best_match_distance = 999999;
-    foreach($pis as $pi) {
-        $dist = levenshtein($url, $pi->url);
-        if($dist < $best_match_distance) {
-            $best_match_distance = $dist;
-            $best_match_pi = $pi;
-        }
-    }
-    if($best_match_pi) {
-        $best_match_url = get_relative_uri($url, $best_match_pi->url);
-        echo 'Did you mean to visit <a href="' . $best_match_url .'">' .$best_match_pi->name . '</a>?';
-    }
-
-    // list all available pages
-    if(PHEASEL_ENVIRONMENT==PHEASEL_ENVIRONMENT_DEV) {
-        echo "<p>Requested URL was: <strong>$url</strong></p>";
-        echo "<p>Available URLs are:</p><ul>";
-        foreach(SiteConfig::get_instance()->get_all_page_infos() as $pi) {
-            if($pi->url) {
-                echo "<li><strong>$pi->url</strong> (id: $pi->id)</li>";
+    if(isset($url)) {
+        $pis = SiteConfig::get_instance()->get_all_page_infos();
+        $best_match_pi = null;
+        $best_match_distance = 999999;
+        foreach($pis as $pi) {
+            $dist = levenshtein($url, $pi->url);
+            if($dist < $best_match_distance) {
+                $best_match_distance = $dist;
+                $best_match_pi = $pi;
             }
         }
-        echo "</ul>";
+        if($best_match_pi) {
+            $best_match_url = get_relative_uri($url, $best_match_pi->url);
+            echo 'Did you mean to visit <a href="' . $best_match_url .'">' .$best_match_pi->name . '</a>?';
+        }
+
+        // list all available pages
+        if(PHEASEL_ENVIRONMENT==PHEASEL_ENVIRONMENT_DEV) {
+            echo "<p>Requested URL was: <strong>$url</strong></p>";
+            echo "<p>Available URLs are:</p><ul>";
+            foreach(SiteConfig::get_instance()->get_all_page_infos() as $pi) {
+                if($pi->url) {
+                    echo "<li><strong>$pi->url</strong> (id: $pi->id)</li>";
+                }
+            }
+            echo "</ul>";
+        }
     }
 ?>
 </body>
--- site/misc/page.500.en.php
+++ site/misc/page.500.en.php
@@ -1,9 +1,24 @@
 <?php
 /**
  * Custom 500 page, feel free to change it to fit your needs.
- * Note: by default the error stack trace is only displayed in DEV mode, not to scare off visitors.
+ *
+ * Note (1): by default the error stack trace is only displayed in DEV mode, not to scare off visitors.
+ *
+ * Note (2): When exporting as HTML or PHP, this page will be exported e.g. to 500.html or 500.php, respectively.
+ * But that does not mean that it will automatically be used when an internal server error occurs.
+ *
+ * If your web server has mod_rewrite enabled you need to add an .htaccess file specifying
+ * ErrorDocument 500 /500.html
+ *
+ * If it does not, please get in touch with your hoster, there are probably has file name conventions for 500 pages,
+ * e.g. that they must be called 500.html.
+ *
+ * Note (3): when used as described in (2), you might see broken links or references to javascript/stylesheets on
+ * this page. If this is the case, please edit pheasel-config.php and insert a value for PHEASEL_CONTEXT_PATH, so
+ * that the RequestHandler can insert references relative to the webserver root.
  */
 ?>
+<!DOCTYPE html>
 <html>
 <head>
 	<ph:config id="500" name="Internal Server Error" />
