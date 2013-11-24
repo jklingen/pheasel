e541a8b9660a78b90a1f7e2810fe23177ea4a036
jklingen <jklingen@users.sourceforge.net>
2013-11-24 22:42:51 +0100

    added examples overview page

---
+++ site/examples/page.examples.de.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <ph:config id="examples" name="Beispile" url="/beispiele/" template="examples" />
+</head>
+<body>
+
+<h1>Beispiele für die Internationalisierung</h1>
+<ul>
+    <li><a href="{ph:url pageid=markup-i18n}">Markup-Internationalisierung</a></li>
+    <li><a href="{ph:url pageid=ini-i18n}">INI-Internationalisierung</a></li>
+</ul>
+
+</body>
+</html>
---
+++ site/examples/page.examples.en.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <ph:config id="examples" name="Examples" url="/examples/" template="examples" />
+</head>
+<body>
+
+<h1>Internationalisation examples</h1>
+<ul>
+    <li><a href="{ph:url pageid=markup-i18n}">Markup internationalisation</a></li>
+    <li><a href="{ph:url pageid=ini-i18n}">INI internationalisation</a></li>
+</ul>
+
+</body>
+</html>
